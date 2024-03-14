5227e38a2b5162540548fcf396f8e188f52fdc0c
Wilson Oliveira Junior <Wilsono.junior@outlook.com>
2024-03-14 19:52:46 -0300

    watchlist

--- project 2/commerce/auctions/urls.py
+++ project 2/commerce/auctions/urls.py
@@ -9,5 +9,8 @@ urlpatterns = [
     path("register", views.register, name="register"),
     path("create", views.createListing, name="create"),
     path("displayCategory", views.displayCategory, name="displayCategory"),
-
+    path("listing/<int:id>", views.listing, name="listing"),
+    path("removeWatchlist/<int:id>", views.removeWatchlist, name="removeWatchlist"),
+    path("addWatchlist/<int:id>", views.addWatchlist, name="addWatchlist"),
+    path("watchlist", views.watchlist, name="watchlist"),
 ]
